ef5c68f13b19eee4fbd919697456655c2decf7a6
Ravi Shekhar <rsshekhar27@outlook.com>
2022-01-26 12:34:48 -0600

    Initial commit and then added README.md file, added application properties

---
+++ src/main/java/com/rs/springframework/SpringBootRabbitMQExampleApplication.java
@@ -0,0 +1,13 @@
+package com.rs.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitMQExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRabbitMQExampleApplication.class, args);
+    }
+
+}
